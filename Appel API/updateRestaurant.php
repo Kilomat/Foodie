fe5199adcc7ef0002d74454ddb9d0c5f326f36fe
caidd <saladeettomate@gmail.com>
2016-05-12 21:18:10 +0800

    Add files via upload

---
+++ Appel API/updateRestaurant.php
@@ -0,0 +1,38 @@
+<?php
+
+
+	session_start();
+
+	ini_set('display_errors','on');
+	error_reporting(E_ALL);
+
+	$url_auth = $_SESSION['api'].'restaurants/'.$_SESSION['restaurantId'].'/'.$_SESSION['Token'];
+
+	$places = intval($_POST['places']);
+	$data = array("name" => $_POST['name'], "adress" => $_POST['adress'], "city" => $_POST['city'],
+		"description" => $_POST['description'], "places" => $places);
+
+	$data = json_encode($data);
+
+	function updateInfo($url, $post) {
+	  $ch = curl_init($url);
+	  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");  
+	  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	  curl_setopt($ch, CURLOPT_POSTFIELDS,$post);
+	  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($post)));
+	  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	  $result = curl_exec($ch);
+	  curl_close($ch);
+	  return $result;
+	}
+
+	$info = updateInfo($url_auth, $data);
+	echo $info."<br/></br>";
+
+	
+
+
+
+
+
+?>
